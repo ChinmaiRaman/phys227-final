d1d3da00760650094945651973753bee59434eb8
Chinmai Raman <raman105@mail.chapman.edu>
2016-05-21 19:14:24 +0000

    first commit

---
+++ final.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+"""
+File: final.py
+Copyright (c) 2016 Chinmai Raman
+License: MIT
+Course: PHYS227
+Assignment: Final
+Date: May 21, 2016
+Email: raman105@mail.chapman.edu
+Name: Chinmai Raman
+Description: Final
+"""
+
+from __future__ import division
+import numpy as np
+import matplotlib.pyplot as plt
+
